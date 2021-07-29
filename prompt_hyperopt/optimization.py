02c117e2e77c700695d448d61082911c01353b62
Cenny Wenner <cwenner@gmail.com>
2021-07-29 20:29:02 +0200

    Rename next config array candidate

--- prompt_hyperopt/optimization.py
+++ prompt_hyperopt/optimization.py
@@ -49,15 +49,15 @@ def configuration_space_greedy_climb(
     last_eval = False
     # @TODO consider counting evaluations for iterations instead
     for it in range(max_iterations or 9999999):
-        logger.info("Iteration %i. Current best: %f", it, best_cost)
-        next_best_arr_conf = None
+        logger.info("Iteration %i. Current best: %f.", it, best_cost)
+        next_arr_conf = None
         if it == 0:
-            next_best_arr_conf = np.nan_to_num(best_configuration.get_array())
+            next_arr_conf = np.nan_to_num(best_configuration.get_array())
             change = None
         elif last_eval and random.random() < random_exploration_chance:
             change = None
             next_configuration = random_sampler()
-            next_best_arr_conf = np.nan_to_num(next_configuration.get_array())
+            next_arr_conf = np.nan_to_num(next_configuration.get_array())
         else:
             last_eval = False
             change = (next_hp_index, next_hp_value_index)
@@ -68,18 +68,18 @@ def configuration_space_greedy_climb(
                 next_hp_value_index = 0
             else:
                 if best_arr_conf[next_hp_index] != next_hp_value_index:
-                    next_best_arr_conf = np.copy(best_arr_conf)
-                    next_best_arr_conf[next_hp_index] = next_hp_value_index
+                    next_arr_conf = np.copy(best_arr_conf)
+                    next_arr_conf[next_hp_index] = next_hp_value_index
                 next_hp_value_index += 1
                 if next_hp_value_index == len(hp.choices):
                     next_hp_index += 1
                     next_hp_value_index = 0
             if next_hp_index == len(best_arr_conf):
                 next_hp_index = 0
-        if next_best_arr_conf is not None:
+        if next_arr_conf is not None:
             next_configuration = Configuration(
                 configuration_space,
-                vector=next_best_arr_conf,
+                vector=next_arr_conf,
                 allow_inactive_with_values=True,
             )
             try:
