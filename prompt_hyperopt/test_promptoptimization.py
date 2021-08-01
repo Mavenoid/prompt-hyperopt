8ac4c18a9978d5aed30fb9035fff8b24fed9595e
Cenny Wenner <cwenner@gmail.com>
2021-08-01 22:03:23 +0200

    Add test for prompt GPT generation

---
+++ prompt_hyperopt/test_promptoptimization.py
@@ -0,0 +1,35 @@
+from typing import Any, Callable, Dict, Optional, Tuple
+import numpy as np
+
+import gpt
+import templatedprompt
+
+import pytest
+
+@pytest.fixture
+def singular_qa_trompt():
+    return templatedprompt.TemplatedPrompt(
+        prompt="{{question_prefix}}{{question}}\n{{answer_prefix}}{{answer}}",
+        available_answers=["Yes", "No"], # @TODO drop?
+        options=dict(
+            question_prefix=["Question: "],
+            answer_prefix=["Answer: "],
+        ),
+    )
+
+
+def test_distilgpt2_singular_qa_trompt(singular_qa_trompt):
+    known_values = dict(question="Do there exist red apples?")
+    logprobs_yes = singular_qa_trompt.get_answer_logprobs(
+        engine="distilgpt2",
+        answer="Yes",
+        known_values=known_values
+    )
+    logprobs_no = singular_qa_trompt.get_answer_logprobs(
+        engine="distilgpt2",
+        answer="No",
+        known_values=known_values,
+    )
+    print("Yes logprobs:", logprobs_yes)
+    print("No logprobs:", logprobs_no)
+    assert logprobs_yes["total"] > logprobs_no["total"]
