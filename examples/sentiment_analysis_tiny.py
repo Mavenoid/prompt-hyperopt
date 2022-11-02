ab1e157ae365221a3768ec39da67755bf3a9a9c5
Cenny Wenner <cwenner@gmail.com>
2022-11-02 01:23:22 +0100

    Simplify example using optimize method

--- examples/sentiment_analysis_tiny.py
+++ examples/sentiment_analysis_tiny.py
@@ -55,9 +55,6 @@ trompt = TemplatedPrompt(
 
 Statement: {{sentence}}{{separator}}{{sentiment_label}} {{sentiment}}
 """,
-    available_answers=[
-        "{{answer_positive}}", "{{answer_negative}}", "{{answer_neutral}}"
-    ], # Optional. Do we want this to be {{answer_yes}} or yes?
     options=dict(
         answer_positive=["Positive", "happy", "Positive sentiment", "🙂", "😀"],
         answer_negative=["Negative", "sad", "Negative sentiment", "☹", "😡", "😞"],
@@ -86,35 +83,24 @@ cs = trompt.get_configuration_space()
 
 best_config = None
 for engine in engines:
-    best_config, best_results, best_cost = prompt_hyperopt.optimization.configuration_space_greedy_climb(
-        cs,
-        lambda config: prompt_hyperopt.datasets.evaluate_boolean_dataset(
-            trompt,
-            engine,
-            config,
-            optimize_parameters=True,
-            optimization_loss_name="sqcost",
-            start_index=0,
-            stop_index=len(dev_examples),
-            dataset=dev_examples,
-            dataset_context_field=None,
-            dataset_question_field="sentence",
-            dataset_answer_field="sentiment",
-            # Note: These are trompts and will be filled during optimization.
-            dataset_answer_mapping={
-                "Positive": "{{answer_positive}}",
-                "Negative": "{{answer_negative}}",
-                "Neutral": "{{answer_neutral}}"
-            },
-        ),
-        lambda results: results["sqcost"] * (1 + results["logloss"]),
-        initial_configuration=best_config,
-        early_termination_cost=1e-3,
-        max_iterations=128,
-        verbosity=1,
+    features_field_mapping = {}
+    targets_field_mapping={"sentiment": "sentiment"}
+    targets_value_mapping={
+        "sentiment": {
+            "Positive": "{{answer_positive}}",
+            "Negative": "{{answer_negative}}",
+            "Neutral": "{{answer_neutral}}",
+        }
+    }
+    best_results = trompt.optimize(
+        engine=engine,
+        examples=dev_examples,
+        features_field_mapping=features_field_mapping,
+        targets_field_mapping=targets_field_mapping,
+        targets_value_mapping=targets_value_mapping,
     )
-
-    test_results = prompt_hyperopt.datasets.evaluate_boolean_dataset(
+    best_config = best_results["configuration"]
+    test_results = prompt_hyperopt.dataseteval.evaluate_boolean_dataset(
         trompt,
         engine,
         best_config,
@@ -122,14 +108,9 @@ for engine in engines:
         start_index=0,
         stop_index=len(test_examples),
         dataset=test_examples,
-        dataset_context_field=None,
-        dataset_question_field="sentence",
-        dataset_answer_field="sentiment",
-        dataset_answer_mapping={
-            "Positive": "{{answer_positive}}",
-            "Negative": "{{answer_negative}}",
-            "Neutral": "{{answer_neutral}}"
-        },
+        features_field_mapping=features_field_mapping,
+        targets_field_mapping=targets_field_mapping,
+        targets_value_mapping=targets_value_mapping,
         temperature=best_results["temperature"],
         answer2bias=best_results["answer2bias"],
     )
