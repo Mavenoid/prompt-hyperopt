fd95be0d6b8af318fc2ef3d1a05fa1d10c33dea4
Cenny Wenner <cwenner@gmail.com>
2022-11-02 22:40:52 +0100

    Move README description to setup.py to rely on file

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
 
-setup()
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
